fe1d18efb25346a52d6cdd575f34c803a2d46916
slevin <575720013@qq.com>
2021-07-12 16:32:12 +0800

    xx

---
+++ bins/deploy.sh
@@ -0,0 +1,26 @@
+
+#!/usr/bin/env sh
+#--------------------------------------------
+# 打包后将生成的静态文件push到远程仓库的ghpages分支
+# author：pc
+#--------------------------------------------
+
+remote='git@github.com:slevin57/cxdblog.git'
+
+# 确保脚本抛出遇到的错误
+set -e
+ 
+# 生成静态文件
+yarn build
+ 
+# 进入生成的文件夹
+cd docs/.vuepress/dist
+ 
+git init
+git add -A
+git commit -m 'deploy'
+ 
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
+git push -f $remote master:ghpages
+ 
+cd -
